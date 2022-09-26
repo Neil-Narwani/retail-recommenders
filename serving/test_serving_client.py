44058aaa8f5f06c30eea634e7df103b6b224da24
Anand Narwani <anand_narwani@yahoo.com>
2022-09-26 12:50:08 -0400

    updated public site to use new stacked model

--- serving/test_serving_client.py
+++ serving/test_serving_client.py
@@ -1,11 +1,14 @@
 import json
 import requests
+import itertools
 
-url='http://ec2-107-23-121-67.compute-1.amazonaws.com:8501/v1/models/retail:predict'
+url='http://ec2-3-88-109-135.compute-1.amazonaws.com:8501/v1/models/retail:predict'
 
 with open('test.json', 'r') as testjson:
     data = json.load(testjson)
 
 response = requests.post(url=url, json=data)
-
-print(response.json())
+dictresp = response.json()
+output_2 = dictresp['outputs']['output_2']
+predictions = list(itertools.chain.from_iterable(output_2[0]))
+print(predictions)
--- web/ModelApiRequest.py
+++ web/ModelApiRequest.py
@@ -3,7 +3,7 @@ import itertools
 
 class ModelApiRequest:
     def __init__(self):
-        self.url='http://ec2-107-23-121-67.compute-1.amazonaws.com:8501/v1/models/retail:predict'
+        self.url='http://ec2-3-88-109-135.compute-1.amazonaws.com:8501/v1/models/retail:predict'
         self.querydict = {
             'signature_name': 'serving_default',
             'inputs': {
@@ -15,11 +15,11 @@ class ModelApiRequest:
 
     def Predict(self, item_ids, item_prices, item_departments):
         for x in item_ids:
-            self.querydict['inputs']['context_item_id'][0].append([x])
+            self.querydict['inputs']['context_item_id'][0].append(x)
         for x in item_prices:
-            self.querydict['inputs']['context_item_price'][0].append([x])
+            self.querydict['inputs']['context_item_price'][0].append(x)
         for x in item_departments:
-            self.querydict['inputs']['context_item_department_id'][0].append([x])
+            self.querydict['inputs']['context_item_department_id'][0].append(x)
 
         response = requests.post(url=self.url, json=self.querydict)
         if response.status_code == 200:
